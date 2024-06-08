8763e8cfe38b6c26816148ed18e9039cd4af71f5
Vivekbhand09 <bhandvivek974@gmail.com>
2024-06-08 16:55:02 +0530

    added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <div className="text-center m-4">
+      <p className="dark:text-gray-200 text-gray-700">
+        Made with ❤️ by Vivek
+      </p>
+      <p className="dark:text-gray-300 text-gray-600">
+        Awesome Dashboard Website © 2024
+      </p>
+    </div>
+  </div>
+);
+
+export default Footer;
